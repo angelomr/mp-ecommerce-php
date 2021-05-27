dbe57a20b079efd54e4c9f00ea52e269b4f8d50c
Angelo M. Rodrigues <angelo@wsbrasil.com>
2021-05-27 15:30:13 -0300

    Ajuste

--- webhook.php
+++ webhook.php
@@ -3,19 +3,19 @@ date_default_timezone_set('America/Sao_Paulo');
 
 switch ($_GET['t']) {
     case 'success':
-        echo "<h2>Pagamento Autorizado<h2>";
+        echo "<h2>Pagamento Autorizado</h2>";
         break;
 
     case 'failure':
-        echo "<h2>Pagamento Falhou<h2>";
+        echo "<h2>Pagamento Falhou</h2>";
         break;
 
     case 'pending':
-        echo "<h2>Pagamento Falhou<h2>";
+        echo "<h2>Pagamento Falhou</h2>";
         break;
     
     default:
-        echo "<h2>Pagamento Falhou<h2>";
+        echo "<h2>Pagamento Falhou</h2>";
 }
 
 if (isset($_POST) && count($_POST) > 0) {
